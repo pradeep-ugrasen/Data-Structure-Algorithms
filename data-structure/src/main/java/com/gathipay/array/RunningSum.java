d1b673e843f8a8999765a8e2917cc5318d779fd9
Pradeep Kumar <pradeep.ugrasen@outlook.com>
2025-09-06 20:15:06 +0000

    Sum of the array

---
+++ data-structure/src/main/java/com/gathipay/array/RunningSum.java
@@ -0,0 +1,17 @@
+package com.gathipay.array;
+
+public class RunningSum {
+
+    public static int runningSum(int [] nums){
+        int result =0;
+        for(int i =0; i<nums.length;i++){
+            result +=nums[i];
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int nums [] = {1,3,-4,5,6};
+        System.out.println("Sum Of the array :: "+ RunningSum.runningSum(nums));
+    }
+}
